e049cd2e0e7ae55c6adf8c0f0b27f761d6bad4c5
Yuya Aratani <hrgruri@gmail.com>
2016-03-21 20:23:51 +0900

    add command: todo

---
+++ src/Command/Todo.php
@@ -0,0 +1,76 @@
+<?php
+namespace Hrgruri\Ricca\Command;
+
+use \Hrgruri\Ricca\Exception\CommandException;
+use \Hrgruri\Ricca\Response;
+
+class Todo extends \Hrgruri\Ricca\Command
+{
+
+    public function run($opt, $key)
+    {
+        $result = null;
+        if (preg_match('/([a-z]+)(|\s.*)$/', $opt, $matched) === 1) {
+            // var_dump($matched);
+            if (method_exists($this, "{$matched[1]}Todo")) {
+                $func   = "{$matched[1]}Todo";
+                $result = $this->{$func}(trim($matched[2]));
+            }
+        }
+        return $result;
+    }
+
+    private function addTodo($opt)
+    {
+        $this->user_data[] = $opt;
+        $this->updateJson();
+        return new Response('add');
+    }
+
+    private function clearTodo($opt)
+    {
+        $this->user_data = null;
+        $this->updateJson();
+        return new Response('clear');
+    }
+
+    private function delTodo($opt)
+    {
+        $result = null;
+        if (count($this->user_data) > 0) {
+            $opt    = (int)$opt;
+            $i      = 1;
+            foreach ($this->user_data as $val) {
+                if ($i === $opt) {
+                    unset($this->user_data[$i]);
+                    $this->user_data = array_values($this->user_data);
+                    $this->updateJson();
+                    $result = new Response('del');
+                }
+                $i++;
+            }
+        }
+        return is_null($result)? new Response('undel'): $result;
+    }
+
+    private function deleteTodo($opt)
+    {
+        return $this->delTodo($opt);
+    }
+
+    private function listTodo($opt)
+    {
+        $result = null;
+        if (count($this->user_data) > 0) {
+            $i = 1;
+            $result = '';
+            foreach ($this->user_data as $val) {
+                $result.= "{$i}: $val\n";
+                $i++;
+            }
+        } else {
+            return new Response('empty');
+        }
+        return $result;
+    }
+}
